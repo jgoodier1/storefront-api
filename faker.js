9f643fa00bd0420d8656b206b6611fa29bd92536
Jacob Goodier <jacobgoodier@Jacobs-MacBook-Pro.local>
2020-11-17 13:13:48 -0500

    function to mass add mock data

---
+++ faker.js
@@ -0,0 +1,268 @@
+const faker = require('faker');
+const Product = require('./src/models/product');
+
+exports.postFaker = (req, res) => {
+  Product.insertMany([
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    },
+    {
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: 'https://picsum.photos/400',
+      description: faker.commerce.productDescription(),
+      userId: '5f57a5bfafa31a0d638157d9'
+    }
+  ]).then(result => res.json('done'));
+};
+
+// console.log(product);
